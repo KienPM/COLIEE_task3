bf7ea515140d294f85659eabf131aaef147824bc
kienpm <kienpm2205@gmail.com>
2021-01-17 21:47:39 +0700

    Update text preprocessing: add lemmatization

--- utils/string_util.py
+++ utils/string_util.py
@@ -1,11 +1,15 @@
 """ Create by Ken at 2021 Jan 03 """
 import string
-from nltk.tokenize import word_tokenize
 import re
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 
 table = str.maketrans('', '', string.punctuation)
 number_re = re.compile(r'^\d+([.,-]\d+)*$')
 clause_re = re.compile(r'^\(\d+\)')
+lemmatizer = WordNetLemmatizer()
 
 
 def remove_numbering(line):
@@ -15,21 +19,48 @@ def remove_numbering(line):
     return line
 
 
+def nltk_tag_to_wordnet_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+
+
 def pre_process_text(text):
     """
-    Tokenize, convert to lower case, remove punctuations
+    Tokenize, lemmatize, convert to lower case, remove punctuations
     :type text: str
     :rtype: list[str]
     """
     tokens = word_tokenize(text)
 
+    # lemmatize
+    # # find the POS tag for each token
+    nltk_tagged = pos_tag(tokens)
+    # # tuple of (token, wordnet_tag)
+    wordnet_tagged = map(lambda x: (x[0], nltk_tag_to_wordnet_tag(x[1])), nltk_tagged)
+    lemmatized_tokens = []
+    for word, tag in wordnet_tagged:
+        if tag is None:
+            # if there is no available tag, append the token as is
+            lemmatized_tokens.append(word)
+        else:
+            # else use the tag to lemmatize the token
+            lemmatized_tokens.append(lemmatizer.lemmatize(word, tag))
+
     # convert to lower case
-    tokens = [w.lower() for w in tokens]
+    tokens = [w.lower() for w in lemmatized_tokens]
 
     # remove punctuation from each word
     stripped = [w.translate(table) if not number_re.match(w) else w for w in tokens]
 
-    words = [w for w in stripped if w != '']
+    words = [lemmatizer.lemmatize(w) for w in stripped if w != '']
 
     return words
 
